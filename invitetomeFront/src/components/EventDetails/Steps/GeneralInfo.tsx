7c9842a630b194c8c16edd8c273ae1170b874e17
aguizzo <andres.guizzo@gmail.com>
2025-04-09 21:08:50 +0200

    feat: added hook to GeneralInfo component to manage form state and add Autocomplete for tags and gates

--- invitetomeFront/src/components/EventDetails/Steps/GeneralInfo.tsx
+++ invitetomeFront/src/components/EventDetails/Steps/GeneralInfo.tsx
@@ -1,11 +1,30 @@
-import { Box, TextField, Grid, Typography, Button } from "@mui/material";
+import { useState } from "react";
+import { GeneralData } from "../../../model/EventDataModel/sortKeys/CoreData";
+import {
+  Box,
+  TextField,
+  Grid,
+  Typography,
+  Button,
+  Autocomplete,
+  Chip,
+} from "@mui/material";
 import { useEventDetailContext } from "../EventContext/EventDetailContext";
 
 export const GeneralInfo = () => {
-  
   const { currentEvent } = useEventDetailContext();
-  
-  console.log(currentEvent);
+
+  const [generalInfoForm, setGeneralInfoForm] = useState<GeneralData>({
+    ...currentEvent.core.generalData,
+  });
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setGeneralInfoForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   return (
     <Box component="form" noValidate autoComplete="off">
@@ -16,15 +35,62 @@ export const GeneralInfo = () => {
             Basic Information
           </Typography>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField value={currentEvent.core.generalData.name || ""} fullWidth label="Event Name" variant="outlined" required />
-        </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={4}>
           <TextField
+            value={generalInfoForm.eventCode}
             fullWidth
-            label="Event Tag"
+            label="Event Code"
             variant="outlined"
-            helperText="Short identifier for your event"
+            required
+            onChange={handleOnChange}
+            name="eventCode"
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <TextField
+            value={generalInfoForm.name}
+            fullWidth
+            label="Event Name"
+            variant="outlined"
+            required
+            onChange={handleOnChange}
+            name="name"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Autocomplete
+            multiple
+            options={[]}
+            freeSolo
+            value={generalInfoForm.tags}
+            onChange={(_event, newValue) => {
+              setGeneralInfoForm((prevState) => ({
+                ...prevState,
+                tags: newValue,
+              }));
+            }}
+            renderTags={(value: string[], getTagProps) =>
+              value.map((option, index) => (
+                <Chip
+                  {...getTagProps({ index })}
+                  sx={{
+                    bgcolor: "#2196f3",
+                    color: "white",
+                    fontWeight: "bold",
+                  }}
+                  key={option}
+                  label={option}
+                />
+              ))
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Tags"
+                variant="outlined"
+                placeholder="Add tags"
+              />
+            )}
           />
         </Grid>
 
@@ -35,21 +101,77 @@ export const GeneralInfo = () => {
           </Typography>
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField fullWidth label="Venue Name" variant="outlined" required />
+          <TextField
+            value={generalInfoForm.venue}
+            fullWidth
+            label="Venue Name"
+            variant="outlined"
+            required
+            onChange={handleOnChange}
+            name="venue"
+          />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField fullWidth label="Phone" variant="outlined" type="tel" />
+          <TextField
+            value={generalInfoForm.city}
+            fullWidth
+            label="City"
+            variant="outlined"
+            type="text"
+            required
+            onChange={handleOnChange}
+            name="city"
+          />
         </Grid>
         <Grid item xs={12}>
           <TextField
+            value={generalInfoForm.address}
             fullWidth
             label="Address"
             variant="outlined"
             required
             multiline
             rows={2}
+            onChange={handleOnChange}
           />
         </Grid>
+        <Grid item xs={12}>
+          <Autocomplete
+            multiple
+            options={[]}
+            freeSolo
+            value={generalInfoForm.gates}
+            onChange={(_event, newValue) => {
+              setGeneralInfoForm((prevState) => ({
+                ...prevState,
+                gates: newValue,
+              }));
+            }}
+            renderTags={(value: string[], getTagProps) =>
+              value.map((option, index) => (
+                <Chip
+                  {...getTagProps({ index })}
+                  sx={{
+                    bgcolor: "#2196f3",
+                    color: "white",
+                    fontWeight: "bold",
+                  }}
+                  key={option}
+                  label={option}
+                />
+              ))
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Gates"
+                variant="outlined"
+                placeholder="Add gates"
+              />
+            )}
+          />
+        </Grid>
+
         {/* Event Logo Upload */}
         <Grid item xs={12}>
           <Typography variant="h6" sx={{ mb: 2, mt: 2 }}>
