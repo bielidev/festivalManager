92b70f82d6b98d86775c6f5a64b4648e1bf0bc62
aguizzo <andres.guizzo@gmail.com>
2025-04-11 22:57:05 +0200

    fix: standardize formatting in Core.ts

--- invitetomeFront/src/model/EventItemModel/Core.ts
+++ invitetomeFront/src/model/EventItemModel/Core.ts
@@ -1,59 +1,62 @@
-
 export interface Core {
-    eventId: string;
-    operation: "core",
-    data: {
-        coreData: {
-            generalData: GeneralData,
-            venueData: VenueData
-        },
-        coreQuotas: {
-            /* Note to myself:
+  eventId: string;
+  operation: "core";
+  data: {
+    coreData: {
+      generalData: GeneralData;
+      venueData: VenueData;
+    };
+    coreQuotas: {
+        /* Note to myself:
               In step 4 of the stepper we create quotas/invitations types,
               and in step 5 we create bundles with those quotas/invitations types.
               In both steps we assing a quantity. Which one is the correct one?
                 If we assign a quantity in step 4, every bundle will have the same quantity.
                 If we assign a quantity in step 5, every bundle can have a different quantity
-            */
-            quotas: Quota[];
-            totalInvitations: number;
-            remainingInvitations: number;
-        }
-        coreStatus: {
-            status: EventStatus;
-        }
-        coreEventDates: string[]; // dates in ISO 8601 format
-    }
+        */
+      quotas: Quota[];
+      totalInvitations: number;
+      remainingInvitations: number;
+    };
+    coreStatus: {
+      status: EventStatus;
+    };
+    coreEventDates: string[]; // dates in ISO 8601 format
+  };
 }
 
-export type EventStatus = "Upcoming" | "Draft" | "Active" | "In Progress" | "Archived"; 
-
+export type EventStatus =
+  | "Upcoming"
+  | "Draft"
+  | "Active"
+  | "In Progress"
+  | "Archived";
 
 export interface GeneralData {
-    eventName: string;
-    description: string;
-    type: string;
-    edition: string;
-    yearEdition: number;
-    websiteUrl: string;
-    logoUrl: string;
-    previewImageUrl: string;
-    phone: string;
-    tags: string[];
+  eventName: string;
+  description: string;
+  type: string;
+  edition: string;
+  yearEdition: number;
+  websiteUrl: string;
+  logoUrl: string;
+  previewImageUrl: string;
+  phone: string;
+  tags: string[];
 }
 
 export interface VenueData {
-    venueName: string;
-    address: string;
-    city: string;
-    country: string;
-    postalCode: string;
-    gates: string[];
+  venueName: string;
+  address: string;
+  city: string;
+  country: string;
+  postalCode: string;
+  gates: string[];
 }
 
 export interface Quota {
-    quotaType: string;
-    quotaQuantity: number;
-    color: string; // color in hexadecimal
-    description: string;
+  quotaType: string;
+  quotaQuantity: number;
+  color: string; // color in hexadecimal
+  description: string;
 }
