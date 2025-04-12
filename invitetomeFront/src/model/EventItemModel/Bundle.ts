8ebf49d572c19a28db9801d79ca3bab85a57ac9e
aguizzo <andres.guizzo@gmail.com>
2025-04-12 09:54:35 +0200

    refactor: Added BundleData and BundleQuotas interfaces for simplifying Bundle item model

--- invitetomeFront/src/model/EventItemModel/Bundle.ts
+++ invitetomeFront/src/model/EventItemModel/Bundle.ts
@@ -6,20 +6,11 @@ export interface Bundle {
   operation: string; // "e.g. bundle#01#cocacola"
   contacts: Contact[];
   data: {
-    bundleData: {
-      bundleName: string;
-      sponsorName: string;
-      bundleDescription: string;
-    };
+    bundleData: BundleData;
     bundleDates: {
       dates: string[];
     };
-    bundleQuotas: {
-      quotas: Quota[];
-      totalInvitations: number;
-      sentInvitations: number;
-      acceptedInvitations: number;
-    };
+    bundleQuotas: BundleQuotas;
     bundleStatus: {
       status: BundleStatus;
     };
@@ -33,4 +24,17 @@ export type BundleStatus = "Draft" | "Active" | "In Progress" | "Archived";
 export interface Contact {
   name: string;
   email: string;
+}
+export interface BundleData {
+  bundleName: string;
+  sponsorName: string;
+  sponsorEmail: string;
+  bundleDescription: string;
+}
+
+export interface BundleQuotas {
+  quotas: Quota[];
+  totalInvitations: number;
+  sentInvitations: number;
+  acceptedInvitations: number;
 }
