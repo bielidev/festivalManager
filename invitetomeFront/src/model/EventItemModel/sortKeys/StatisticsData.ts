1ec4bec9d7bab5657d6fdafc20149842bc72fc6c
aguizzo <andres.guizzo@gmail.com>
2025-04-12 17:44:55 +0200

    refactor: Remove old event model data

--- invitetomeFront/src/model/EventItemModel/sortKeys/StatisticsData.ts
+++
@@ -1,9 +0,0 @@
-export interface Statistics {
-    statisticsData: StatisticsData;
-  }
-  
-  export interface StatisticsData {
-    openRate: number;
-    totalScans: number;
-    attendanceRate: number;
-  }
