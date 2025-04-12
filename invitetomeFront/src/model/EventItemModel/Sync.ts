e9f87bc9b53099dcfd2493cd16b89a78a9c012ba
aguizzo <andres.guizzo@gmail.com>
2025-04-12 09:22:38 +0200

    feat: Add Sync item model

---
+++ invitetomeFront/src/model/EventItemModel/Sync.ts
@@ -0,0 +1,19 @@
+export interface Sync {
+  eventId: string;
+  operation: "sync";
+  data: {
+    syncData: {
+      timestamps: Timestamps;
+    };
+  };
+}
+
+/* Timestamps are in ISO 8601 format */
+export interface Timestamps {
+  core: string;
+  bundles: string;
+  artists: string;
+  statistics: string;
+  // For individual bundles and invitations items
+  [key: string]: string;
+}
